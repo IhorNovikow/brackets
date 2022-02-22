3652b4ff394f947d78ab44283bbc55faa8189061
IhorNovikow <gosha.noviko2012@gmail.com>
2022-02-22 13:53:54 +0300

    doc: create full task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, bracketsConfig) {
+  let stak = [];
+ let brackets={
+    ')':'(',
+    '}':'{',
+    ']':'[',
+  };
+
+ for(let i = 0; i < s.length; i++) {
+   const current = s[i];
+  if(isClosedBracket(current)){
+
+    if (brackets[current] !== stak.pop()) return false;
+  }else{
+    stak.push(current)
+  }
+ }
+return stak.length === 0; 
 }
+
+
+function isClosedBracket(ch){
+  return [')', '}', ']'].indexOf(ch) > -1;
+}
+/*function isValid(s){
+let stak = [];
+ let brackets={
+    ')':'(',
+    '}':'{',
+    ']':'[',
+  };
+
+ for(let i = 0; i < s.length; i++) {
+   const current = s[i];
+  if(isClosedBracket(current)){
+
+    if (brackets[current] !== stak.pop()) return false;
+  }else{
+    stak.push(current)
+  }
+ }
+return stak.length === 0; 
+}
+
+
+function isClosedBracket(ch){
+  return [')', '}', ']'].indexOf(ch) > -1;
+}*/
